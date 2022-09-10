2c16911efd66389a339b32ebf4fe56d92a5bb555
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-09-10 21:27:50 +0500

    Implemented Kite animation

---
+++ Assets/Scripts/KiteAnimation.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class KiteAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private  Vector3 start_pont;
+    [SerializeField] private Vector3 end_point;
+    [SerializeField] private float fall_time = 9;
+    [SerializeField] private int curves = 5;
+    [SerializeField] private float amplitude = 0.5f;
+
+
+    void Start() {
+        
+        curves += 2;
+
+        Vector3[] points = new Vector3[curves];
+        
+        transform.position = start_pont;
+
+
+        // Set points for the animation
+        float distance = end_point.y - start_pont.y;
+
+        float interval = distance / (curves - 1);
+
+        points[0] = transform.position;
+
+        Vector3 inter_pos = new Vector3(0, transform.position.y, transform.position.z);
+
+        for (int i = 1; i < points.Length - 1; i++) {
+            
+            // Set the x_coordinate for interval
+            if (i % 2 == 0) 
+                inter_pos.x = transform.position.x + amplitude;
+            else
+                inter_pos.x = transform.position.x - amplitude;
+
+            // Set the y component for interval
+            inter_pos.y += interval;
+
+            points[i] = inter_pos;
+        }
+
+        points[points.Length - 1] = end_point;
+
+        // Perform the animation on the points
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(
+            transform?.DOPath(points, fall_time, PathType.CatmullRom)
+        );
+    }
+}
